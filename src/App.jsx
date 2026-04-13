3b608a2f5a24309d877f4c57be6d14f44d590ddf
Pravin Kumar <kumar.pravin160@gmail.com>
2026-04-13 18:39:17 +0530

    Restructure layout to two-column with logo/content on left and form on right

--- src/App.jsx
+++ src/App.jsx
@@ -11,34 +11,39 @@ function App() {
       <div className="video-bg">
         <img src={cominf} alt="Background" />
       </div>
-      <div className="container">
-        <div className="logo">
-          <img src={logo} alt="Web4You Logo" />
-          <img src={hiddenLogo} style={{display: 'none'}} alt="Web4You Logo" />
-        </div>
-
-        <p>Websites | Apps | Marketing | Complete Digital Solutions</p>
-
-        <LeadForm />
-
-        <a href="#" target="_blank" className="btn">
-          Follow Us
-        </a>
-
-        {/* 🔥 Social Icons */}
-        <div className="social">
-          <a href="https://www.facebook.com/web4younoida/" target="_blank">
-            <i className='bx bxl-facebook'></i>
+      <div className="main-layout">
+        {/* Left Side - Logo & Content */}
+        <div className="left-content">
+          <div className="logo">
+            <img src={logo} alt="Web4You Logo" />
+            <img src={hiddenLogo} style={{display: 'none'}} alt="Web4You Logo" />
+          </div>
+          <p className="tagline">Websites | Apps | Marketing | Complete Digital Solutions</p>
+          
+          <a href="#" target="_blank" className="btn">
+            Follow Us
           </a>
-          <a href="https://www.linkedin.com/company/web4you-noida/" target="_blank">
-            <i className='bx bxl-linkedin'></i>
-          </a>
+
+          {/* 🔥 Social Icons */}
+          <div className="social">
+            <a href="https://www.facebook.com/web4younoida/" target="_blank">
+              <i className='bx bxl-facebook'></i>
+            </a>
+            <a href="https://www.linkedin.com/company/web4you-noida/" target="_blank">
+              <i className='bx bxl-linkedin'></i>
+            </a>
+          </div>
         </div>
 
-        <div className="footer">
-          📞 <a href="tel:8090212521"> +91 80902 12521 </a> | <a href="tel:+919971411966"> +91 9971411966 </a> <br />
-          © 2026 Web4You. All Rights Reserved.
+        {/* Right Side - Form */}
+        <div className="right-form">
+          <LeadForm />
         </div>
+      </div>
+
+      <div className="footer">
+        📞 <a href="tel:8090212521"> +91 80902 12521 </a> | <a href="tel:+919971411966"> +91 9971411966 </a> <br />
+        © 2026 Web4You. All Rights Reserved.
       </div>
     </>
   );
